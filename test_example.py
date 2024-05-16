2e7dc6523d34ab378a0d606473382ebf1d984384
Fergal Armstrong <148789+fffergal@users.noreply.github.com>
2024-05-16 17:02:41 +0100

    Handle constants, no more import, example tests (#6)

--- test_example.py
+++ test_example.py
@@ -1,96 +1,41 @@
 import doctest
-import os
-import pathlib
-import subprocess
-import tempfile
-from inspect import cleandoc
+import sys
+import unittest
+import unittest.runner
+
+import pillory
 
 
 def doctest_pillory():
     """
     >>> doctest_pillory()
     ... # doctest: +ELLIPSIS +REPORT_UDIFF
-    F.
-    ======================================================================
-    FAIL: test_hey1 (__main__.UseTestCase)
+    definition imported
+    x..
     ----------------------------------------------------------------------
-    Traceback (most recent call last):
-    ...
-    AssertionError: 1 != 2
+    Ran 3 tests in ...
     <BLANKLINE>
-    ----------------------------------------------------------------------
-    Ran 2 tests in ...
-    <BLANKLINE>
-    FAILED (failures=1)
-    <BLANKLINE>
+    OK (expected failures=1)
     .../test_use.py... PM101 ...
     .../test_use.py... PM102 ...
     .../test_use.py... PM103 ...
     """
-    with tempfile.TemporaryDirectory(dir=os.getcwd()) as tmp:
-        tmp_path = pathlib.Path(tmp)
-        def_path = tmp_path / "definition.py"
-        def_path.write_text(
-            cleandoc(
-                """
-                class Hey:
-                    def yo(self):
-                        return 1
-                """
-            )
-        )
-        use_path = tmp_path / "use.py"
-        use_path.write_text(
-            cleandoc(
-                f"""
-                from {tmp_path.name}.definition import Hey
 
-                def do_something():
-                    value = Hey().yo()
-                    print(value)
-                    return value
-                """
-            )
-        )
-        test_path = tmp_path / "test_use.py"
-        test_path.write_text(
-            cleandoc(
-                f"""
-                import unittest
-                import unittest.mock
+    class StdoutTestRunner(unittest.runner.TextTestRunner):
+        def __init__(self, *args, **kwargs):
+            kwargs["stream"] = sys.stdout
+            super().__init__(*args, **kwargs)
 
-                from {tmp_path.name} import use
-
-                @unittest.mock.patch("builtins.print")
-                class UseTestCase(unittest.TestCase):
-                    @unittest.mock.patch("{tmp_path.name}.definition.Hey")
-                    def test_hey1(self, hey, print):
-                        hey.return_value.yo.return_value = 2
-                        self.assertEqual(use.do_something(), 2)
-
-                    @unittest.mock.patch("{tmp_path.name}.definition.Hey.yo")
-                    def test_hey2(self, yo, print):
-                        yo.return_value = 3
-                        self.assertEqual(use.do_something(), 3)
-
-                if __name__ == "__main__":
-                    unittest.main()
-                """
-            )
+    try:
+        unittest.main(
+            "example.test_use", testRunner=StdoutTestRunner, argv=sys.argv[:1]
         )
-        (tmp_path / "__init__.py").touch()
-        unit_proc = subprocess.run(
-            ["python", "-m", f"{tmp_path.name}.test_use"],
-            capture_output=True,
-            text=True,
-        )
-        print(unit_proc.stderr)
-        pillory_proc = subprocess.run(
-            ["python", "-m", "pillory", str(tmp_path.relative_to(os.getcwd()))],
-            capture_output=True,
-            text=True,
-        )
-        print(pillory_proc.stdout)
+    except SystemExit:
+        pass
+    # If pillory imports the module again to lint it, it will run the print statement
+    # inside. We want to prove pillory doesn't import the code it lints.
+    del sys.modules["example.definition"]
+    pillory.main(["example"])
 
 
 if __name__ == "__main__":
